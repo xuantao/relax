a351f06c9de64fb1870acf4bd071c8db9a5f7cd3
xuantao <xuantao.zh@hotmail.com>
2020-07-12 23:53:14 +0800

    test lpeg

--- thrift-lua-exporter/export_thrift_error.lua
+++ thrift-lua-exporter/export_thrift_error.lua
@@ -9,7 +9,7 @@ enum /*<@EnumDef>*/ KickReason {
 ]]
 
 local space = lpeg.S ' \t\n'^0
-local comm = space * lpeg.P'//' * (1-lpeg.P'\n')^0 * lpeg.P'\n'
+local comm = space * lpeg.P'//' * (1-lpeg.P'\n')^0 * lpeg.P'\n'^-1
 local m_comm = space * lpeg.P'/*' * (1 - lpeg.P'*/')^0 * lpeg.P'*/'
 local letter = (lpeg.R'az' + lpeg.R'AZ' + lpeg.R'09' + lpeg.S'_')
 local c_name = lpeg.C(letter^1)
@@ -22,27 +22,6 @@ local c_element = space * c_name * space * c_value^-1 * space * lpeg.S',;' * spa
 
 print("000", table.concat({c_element:match(" xuantao = 123,  // nihao  ")}, '-')..'&')
 print("000", c_element:match(" xuantao = 246 ,"))
-
-
-function anywhere(...)
-    local i = lpeg.Cp()
-    local p = nil
-    for _, key in ipairs({...}) do
-        if not p then
-            p = lpeg.P(key)
-        else
-            p = p + lpeg.P(key)
-        end
-    end
-    return lpeg.P{ i * p * i + 1 * lpeg.V(1) }
-end
-
-local intr = anywhere("enum", "struct", "include")
---local intr = anywhere("enum")
-
-
-
---local 
 
 local text_com = [[
  // xuangag //
@@ -57,66 +36,83 @@ struct {
 }
 
 include ;
+enumstruct
 ]]
 
-local pos = 1
-while true do
-    local l
-    pos, l = intr:match(text_com, pos)
-    print(pos, l)
-    if not pos then
-        break
-    end
-    pos = pos + 1
-end
-
-local s = "/* */"
-print("111", lpeg.match(m_comm, s))
-
-
-local elements = {
-    comm, m_comm
-}
-
-print("222", enum_label:match("/*<@EnumDef>*/"))
-
-print("333", c_enum:match([[
-enum /*<@ErrorCode>*/ LSError {
-}
-]]))
-
-
-
--- 扫描
---print(comm:match(text_com))
-local at = 1
-local i = 0
---[[
-while true do
-    local p
-    for id, pat in ipairs(elements) do
-        p = pat:match(text_com, at)
-        if p then
-            print("id", id)
-            break
+function anywhere(...)
+    local i = lpeg.Cp()
+    local p = nil
+    for _, key in ipairs({...}) do
+        if not p then
+            p = lpeg.P(key)
+        else
+            p = p + lpeg.P(key)
         end
     end
-    if not p then
-        break
-    end
+    return lpeg.P{ i * p * i + 1 * lpeg.V(1) }
+end
 
-    at = p
-    print("at", at)
-    i = i + 1
-    if i > 10 then
-        break
+local function testScan()
+    local intr = anywhere("enum", "struct", "include")
+    local pos = 1
+    while true do
+        local l
+        pos, l = intr:match(text_com, pos)
+        if not pos then
+            break
+        end
+
+        print("intr", pos, l, string.sub(text_com, pos, pos + 10))
+        pos = pos + 1
     end
 end
-]]
---print(text_com:sub(at))
+
+local function genScanner(...)
+    local i = lpeg.Cp()
+    local p = nil
+    for _, key in ipairs({...}) do
+        if not p then
+            p = lpeg.C(key)
+        else
+            p = p + lpeg.C(key)
+        end
+    end
+    return lpeg.P{ i * p * i + 1 * lpeg.V(1) } / function (b, k, e) return k, b, e end
+end
+
+local function testCapture()
+    print("testCapture")
+
+    local scanner = genScanner("//", "enum", "include", "struct")
+    local p = 1
+    while true do
+        local k, b, e = scanner:match(text_com, p)
+        if not k then
+            break
+        end
+
+        p = e
+        print(k, b, e)
+    end
+end
+--testCapture()
 
 
+local function test()
+    local I = lpeg.Cp()
+    local P = comm * I
 
+    local t1 = [[
+        // faldgladjga
+    ]]
 
+    print(P:match("////// xuagakdgka"))
+    print(P:match(t1))
+
+    local P2 = comm / ""
+    print(P2:match("////// xuagakdgka") .. '$')
+    print(P2:match(t1) .. '$')
+end
+test()
 
 
